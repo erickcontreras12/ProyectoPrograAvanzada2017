872c6f24ff1422d43cb39c69616b04ed43dec4e1
GaryEstGt <31348611+GaryEstGt@users.noreply.github.com>
2017-10-07 22:35:41 -0600

    Inicio de clase prestamo por lote

---
+++ ProyectoPrograAvanzada/ProyectoPrograAvanzada/PrestamoporLote.cs
@@ -0,0 +1,14 @@
+﻿using PruebasConsolsa;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoPrograAvanzada
+{
+    class PrestamoporLote
+    {
+       
+    }
+}
